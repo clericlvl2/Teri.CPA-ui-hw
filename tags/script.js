7e08424c4a0c39c7495c3b39477945bf33f1dcc2
Artem Budarnykh <budarnykh.a@yandex.ru>
2022-07-23 01:36:40 +0700

    made tasks 1 2 3

--- tags/script.js
+++ tags/script.js
@@ -1,8 +1,17 @@
+/*
+Добавьте в документ несколько картинок, каждая из которых с уникальным идентификатором, задайте ширину и высоту.
+  Выведите с помощью JS кол-во ссылок
+Покажите содержимое (innerHTML) первого ссылки на странице
+Выведите количество картинок
+Выведите ширину первой картинки
+Выведите ширину самой широкой картинки
+Выведите сумму всех высот картинок*/
+
 let links = document.querySelectorAll('.content__link');
 let linkContent = links[0].innerHTML;
 let linksNum = links.length;
 
-function getMaxWidth(pics) {
+function getMaxWidth (pics) {
   let temp = null;
   for (let pic of pics) {
     let width = pic.getBoundingClientRect().width;
@@ -10,8 +19,7 @@ function getMaxWidth(pics) {
   }
   return temp;
 }
-
-function getSumHeight(pics) {
+function getSumHeight (pics) {
   let temp = null;
   for (let pic of pics) {
     temp += pic.getBoundingClientRect().height;
@@ -21,10 +29,11 @@ function getSumHeight(pics) {
 }
 
 let pics = document.querySelectorAll('.content__image');
-let picsNum = pics.length;
+let picsNum = pics.length
 let picWidth = pics[0].getBoundingClientRect().width;
-let picMaxWidth = getMaxWidth(pics);
-let picSumHeight = getSumHeight(pics);
+let picMaxWidth = getMaxWidth (pics);
+let picSumHeight = getSumHeight (pics);
+//let picHeightSum = pics.reduce((a,b) => a.getBoundingClientRect().height + b.getBoundingClientRect().height, 0);
 
 document.getElementById('link_num').textContent = linksNum;
 document.getElementById('link_content').textContent = linkContent;
--- url/script.js
+++ url/script.js
@@ -1,10 +1,11 @@
-//const url = new URL (window.location.href);
-const url = new URL ('https://example.com/path1/path2/index.html?name=Jonathan%20Smith&age=18');
+const url = new URL (window.location.href);
+//const url = new URL ('https://example.com/path1/path2/index.html?name=Jonathan%20Smith&age=18');
 
-function showUrl (protocol, host, pathname, search, hash) {
+function showUrlData (protocol, host, pathname, search, hash) {
   // cut path name for path and filename
   let path = pathname.slice(0, pathname.lastIndexOf('/')+1);
   let filename = pathname.slice(pathname.lastIndexOf('/')+1);
+
   document.getElementById('protocol').textContent = protocol;
   document.getElementById('host').textContent = '//' + host
   document.getElementById('path').textContent = path;
@@ -12,26 +13,17 @@ function showUrl (protocol, host, pathname, search, hash) {
   document.getElementById('search').textContent = search;
   document.getElementById('hash').textContent = hash;
 }
-showUrl (url.protocol, url.host, url.pathname, url.search, url.hash);
+showUrlData (url.protocol, url.host, url.pathname, url.search, url.hash);
 
 function getUrlParam (url) {
-  let parameters = {}
+  let urlParam = {}
   for (let [key, value] of url.searchParams) {
-    parameters[key] = value;
+    urlParam[key] = value;
   }
-  return parameters;
+  console.log("URL parameters:")
+  console.log(urlParam);
 }
+getUrlParam (url);
 
-function showUrlData (protocol, pathname, parameters) {
-  let filename = pathname.slice(pathname.lastIndexOf('/')+1);
-  let file = filename.slice(0, filename.lastIndexOf('.'));
-  let ext = filename.slice(filename.lastIndexOf('.'));
 
-  document.getElementById('data-protocol').textContent = protocol;
-  document.getElementById('data-file').textContent = file;
-  document.getElementById('data-ext').textContent = ext;
-  for (const [key, value] of Object.entries(parameters)) {
-    document.getElementById('data-param').textContent += (`${key}: ${value}, `);
-  }
-}
-showUrlData(url.protocol, url.pathname, getUrlParam (url));
+
